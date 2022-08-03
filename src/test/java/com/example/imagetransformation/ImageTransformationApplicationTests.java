d2759edb903bf3b32f930f7238128bf01f616aa5
Ylan <99525156+YlanNaly@users.noreply.github.com>
2022-08-03 17:16:58 +0300

    Add files via upload

---
+++ src/test/java/com/example/imagetransformation/ImageTransformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imagetransformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageTransformationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
